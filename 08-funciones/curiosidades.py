04e03a91025c6e77abc7415e5ff3bfb6da8ba457
Javier-Checa <javierchecaniles@hotmail.com>
2026-09-05 20:12:25 +0200

    recomendaciones

---
+++ 08-funciones/curiosidades.py
@@ -0,0 +1,17 @@
+def mi_funcion():
+    return "Buenas, soy una función"
+
+def mi_segunda_funcion():
+    return "¿Qué hay de nuevo, viejo?"
+
+nombre = "Javier"
+apellido = "Checa"
+
+
+print(mi_funcion() + ", " + nombre + " " + apellido)
+print(mi_segunda_funcion() + "," + " Sr." + " " + apellido)
+
+
+print("Hola, Earl")
+print("Hola, " + nombre + " " + apellido)
+
