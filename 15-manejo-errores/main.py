ca659db82aca3d0570793e0a10bc4515b286f5a9
Javier-Checa <javierchecaniles@hotmail.com>
2026-09-21 17:37:12 +0200

    Manejo de errores 01

--- 15-manejo-errores/main.py
+++ 15-manejo-errores/main.py
@@ -3,5 +3,15 @@ Capturar excepciones y manejar errores en código
 susceptible a fallos/errores
 """
 
-nombre = input("¿Cuál es tu nombre?: ")
-print(nombre)
+try:
+    
+    nombre = input("¿Cuál es tu nombre?: ")
+
+    if len(nombre) > 1:
+        
+        nombre_usuario = "El nombre es " + nombre
+
+    print(nombre_usuario)
+
+except:
+    print("Ha ocurrido un error. Introduce un nombre, por favor.")
