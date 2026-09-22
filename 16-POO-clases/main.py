d66d8c1d8f9b966f42a129644242bf078ffdbf23
Javier-Checa <javierchecaniles@hotmail.com>
2026-09-22 20:02:51 +0200

    POO inicio

---
+++ 16-POO-clases/main.py
@@ -0,0 +1,35 @@
+# Programación orientada a objetos (POO u OOP)
+
+# Definir una clase (molde para crear más objetos de ese tipo)
+# (Coche) con características similares
+
+class Coche:
+
+        # Atributos o propiedades (variables)
+        # Carcacterísticas del coche
+        color = "Rojo"
+        marca = "Ferrari"
+        modelo = "Aventador"
+        velocidad = 300
+        caballaje = 500
+        plazas = 2
+
+        # Métodos, que son acciones que hace el objeto (coche). Son las antiguas funciones.
+        
+        def acelerar(self):
+            self.velocidad += 1
+
+        def frenar(self):
+            self.velocidad -= 1
+
+        def getVelocidad(self):
+            return self.velocidad
+
+# Fin definicion clase
+
+# Crear objetos / Instanciar la clase
+
+coche = Coche()
+
+print(coche.marca)
+print(coche.modelo)
