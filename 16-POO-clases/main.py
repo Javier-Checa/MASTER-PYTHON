e1b346fdb32b963772671acb8d50668b142f63bf
Javier-Checa <javierchecaniles@hotmail.com>
2026-09-23 14:32:27 +0200

    POO inicio 01+

--- 16-POO-clases/main.py
+++ 16-POO-clases/main.py
@@ -33,3 +33,21 @@ coche = Coche()
 
 print(coche.marca)
 print(coche.modelo)
+
+# Fin definicion clase
+
+# Crear objetos / Instanciar la clase
+
+coche = Coche()
+
+print(coche.marca, coche.modelo, coche.color)
+print("Velocidad actual: ", coche.velocidad)
+
+coche.acelerar()
+coche.acelerar()
+coche.acelerar()
+coche.acelerar()
+coche.frenar()
+
+
+print("Velocidad nueva: ", coche.velocidad)
