26ba2e242a214993f55571aa704530a9def67fb9
Javier-Checa <javierchecaniles@hotmail.com>
2026-09-24 20:16:17 +0200

    POO constructores

---
+++ 17-POO-constructor/coche.py
@@ -0,0 +1,69 @@
+class Coche:
+
+        # Atributos o propiedades (variables)
+        # Carcacterísticas del coche
+        color = "Rojo"
+        marca = "Ferrari"
+        modelo = "Aventador"
+        velocidad = 300
+        caballaje = 500
+        plazas = 2
+        
+        """
+        Definición del constructor: método especial dentro de una clasees un método especial 
+        de una clase que se ejecuta automáticamente cuando se crea (instancia) 
+        un nuevo objeto de esa misma clase.
+        Su función principal es inicializar el objeto, es decir, asignarle un estado inicial válido
+        estableciendo los primeros valores de sus atributos antes de que 
+        el programa empiece a utilizarlo.
+        """
+        
+        def __init__(self, color, marca, modelo, velocidad, caballaje, plazas):
+            self.color = color
+            self.marca = marca
+            self.modelo = modelo
+            self.velocidad = velocidad
+            self.caballaje = caballaje
+            self.plazas = plazas
+
+        # Métodos, que son acciones que hace el objeto (coche). Son las antiguas funciones.
+        
+        def setColor(self, color):
+            self.color = color
+            
+        def getColor(self):
+            return self.color
+        
+        def setModelo(self, modelo):
+            self.modelo = modelo
+            
+        def getModelo(self):
+            return self.modelo
+        
+        def setMarca(self, marca):
+            self.marca = marca
+            
+        def getMarca(self):
+            return self.marca
+                
+        def acelerar(self):
+            self.velocidad += 3 
+
+        def frenar(self):
+            self.velocidad -= 2
+
+        def getVelocidad(self):
+            return self.velocidad
+
+        def getInfo(self):
+            
+            info = "---- Información del coche ----"
+            info += "\n Color: " + self.getColor()
+            info += "\n Marca: " + self.getMarca()
+            info += "\n Modelo: " + self.getModelo()
+            info += "\n Velocidad: " + str(self.getVelocidad())
+            
+            return info
+            
+            
+# Fin definición de clase            
---
+++ 17-POO-constructor/main.py
@@ -0,0 +1,20 @@
+from coche import Coche
+
+carro = Coche("Amarillo", "Renault", "Clio", 150, 220, 4)
+carro1 = Coche("Rojo", "Citroën", "XSara", 160, 230, 5)
+carro2 = Coche("Gris oscuro", "Seat", "Leon", 180, 240, 5)
+carro3 = Coche("Azul metalizado", "Volkswagen", "Polo", 200, 250, 5)
+
+
+
+
+print(carro.getInfo())
+print(carro1.getInfo())
+print(carro2.getInfo())
+print(carro3.getInfo())
+
+
+
+
+
+
