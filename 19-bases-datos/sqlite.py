ebe97bb610318acea3150acf53b04bbab338d4f1
Javier-Checa <javierchecaniles@hotmail.com>
2026-09-26 18:58:07 +0200

    Bases de Datos: SQLite

--- 19-bases-datos/sqlite.py
+++ 19-bases-datos/sqlite.py
@@ -14,12 +14,14 @@ cursor = conexion.cursor()
 
 # Crear tabla
 
-cursor.execute("CREATE TABLE IF NOT EXISTS productos("+
-"id INTEGER PRIMARY KEY AUTOINCREMENT, "+
-"titulo varchar(255), "+
-"descripcion text, "+
-"precio int(255)"+               
-")")
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS productos(
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    titulo varchar(255),
+    descripcion text,
+    precio int(255)              
+)
+""")
 
 
 # Guardar cambios
@@ -28,8 +30,10 @@ conexion.commit()
 
 
 # Insertar datos
+"""
 cursor.execute("INSERT INTO productos VALUES (null, 'Primer producto', 'Descripción de mi producto', 550)")
 conexion.commit()
+"""
 
 # Listar datos
 cursor.execute("SELECT * FROM productos;")
@@ -41,9 +45,9 @@ for producto in productos:
     print("Precio:", producto[3])
     print("\n")
 
-
-
-
+cursor.execute("SELECT titulo FROM productos;")    
+producto = cursor.fetchone()
+print(producto)
 
 # Cerrar conexión
 
